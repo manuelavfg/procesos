ddf2ea14fca1ac08e0350d0c483a5b56a0cf5224
Oswaldo <borgesoswaldo6@gmail.com>
2025-02-25 15:05:00 -0400

    f

--- src/app/pages/config/config.component.ts
+++ src/app/pages/config/config.component.ts
@@ -13,12 +13,14 @@ import { APIService } from '../../api.service';
 })
 export class ConfigComponent {
 tasa = 'tasa'
+iva = 'iva'
 nombre = ' Suministros eléctricos del Zulia, C.A (Sumelzuca)'
 correo = 'sumelzuca@gmail.com'
 logo = new Image()
 logoSrc = '/assets/img/sumelzucalogo.png';
 rif = 'J-50206515-6'
 crltvfactura = 'FACT-2025'
+telefono = '0261-7920000'
 crltvpresupuesto = 'PRES-2025'
 
 constructor(private api: APIService) {
