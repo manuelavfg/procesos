359eaa486ab7561244b77de42a7c3efd08c24bab
Oswaldo <borgesoswaldo6@gmail.com>
2025-02-25 10:49:43 -0400

    config

--- src/app/pages/config/config.component.ts
+++ src/app/pages/config/config.component.ts
@@ -3,6 +3,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
+import { APIService } from '../../api.service';
 
 @Component({
   selector: 'app-config',
@@ -11,5 +12,21 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './config.component.scss'
 })
 export class ConfigComponent {
+tasa = 'tasa'
+correo = 'correo'
+logo = new Image()
+logoSrc = '/assets/img/sumelzucalogo.png';
+rif = 'rif'
+crltvfactura = 'crltvfactura'
+crltvpresupuesto = 'crltvpresupuesto'
+
+constructor(private api: APIService) {
+
+  this.logo.src = '/assets/img/sumelzucalogo.png'
+  this.logo.loading = 'lazy'
+  this.logo.width = 200
+  this.logo.height = 200
+
+}
 
 }
